3f46a1520f131ef203e2a56d2523a7cc3c84e518
lingadevaru <lingadevaruc5@gmail.com>
2019-05-02 13:28:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/updatedspinnerapp/Config.java
@@ -0,0 +1,15 @@
+package com.example.updatedspinnerapp;
+
+public class Config {
+    public static final String DATA_URL = "https://api.myjson.com/bins/x38s0";
+
+    //Tags used in the JSON String
+    public static final String TAG_HEALTHPLAN = "healthplan";
+    public static final String TAG_YEAR = "year";
+    public static final String TAG_MONTH = "month";
+    public static final String TAG_CAPAMOUNT = "capamount";
+
+    //JSON array name
+    public static final String JSON_ARRAY = "result";
+
+}
